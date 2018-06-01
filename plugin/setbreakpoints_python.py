346e9f3e52e658e243a510a9f70c69f467aa3761
sillybun <zhangytfudan@gmail.com>
2018-06-01 13:44:18 +0800

    updata

--- plugin/setbreakpoints_python.py
+++ plugin/setbreakpoints_python.py
@@ -1,16 +1,24 @@
-from pdb import set_trace
 import re
 
 
 def create_import(code, pdb):
     if pdb:
-        if 'from pdb import set_trace' in code:
-            return (code,   False)
-        return (["from pdb import set_trace"] + code, True)
+        spdb = 'pdb'
     else:
-        if 'from pudb import set_trace' in code:
-            return (code,   False)
-        return (["from pudb import set_trace"] + code, True)
+        spdb = 'ipdb'
+    if "from {} import set_trace".format(spdb) in code:
+        return (code,   False)
+    newcode = [""]
+    flag = False
+    for row in code:
+        if flag:
+            newcode = newcode + [row]
+        elif len(row) != 0 and row[0] == '#':
+            newcode = newcode + [row]
+        else:
+            newcode = newcode + ["from {} import set_trace".format(spdb)] + [row]
+            flag = True
+    return (newcode[1:], True)
 
 
 def remove_import(code, pdb):
@@ -20,7 +28,7 @@ def remove_import(code, pdb):
     if pdb:
         return ([row for row in code if row != 'from pdb import set_trace'], True)
     else:
-        return ([row for row in code if row != 'from pudb import set_trace'], True)
+        return ([row for row in code if row != 'from ipdb import set_trace'], True)
 
 
 def remove_set_trace(code, pdb):
--- plugin/test.py
+++ plugin/test.py
@@ -1,3 +1,6 @@
+# this is
+#!/bin/python
+
 import os
 
 for i in range(10):
